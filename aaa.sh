751b980d7bfd8355e8983a79b747824ebcd3f37e
zhaosk <kangles@qq.com>
2026-01-10 19:48:06 +0800

    a

--- aaa.sh
+++ aaa.sh
@@ -1,9 +1,58 @@
-dfasfasfdsafd
-dfasfasfdsafdgds
-gs
-gfdsg
-dfasfasfdsafdgdsdsg
-dfasfasfdsafdgdsdsggs
-gsdfg
-gsdfg
-dfasfdafasfdsfsdfadfadf
+ @Override
+    public BidStatusVo queryStatus(BidQueryStatus queryStatus) {
+        Long projectId = projectService.findProjectIdByBdhBjbhZbid(queryStatus.getBjbh(), queryStatus.getBdh(), queryStatus.getZbid());
+        if (projectId == null || projectId <= 0) {
+            throw new RuntimeException("项目不存在");
+        }
+        List<SectionStatusVo> sectionStatusVos = evaluationResultService.selectSectionStatusVoListByProjectId(projectId);
+        BidStatusVo bidStatusVo = new BidStatusVo();
+        BeanUtils.copyProperties(queryStatus, bidStatusVo);
+        bidStatusVo.setSectionStatusList(sectionStatusVos);
+        return bidStatusVo;
+    }
+
+    @Override
+    public BidScoreVo queryByTkbm(BidScoreQuery scoreQuery) {
+        Long projectId = projectService.findProjectIdByBdhBjbhZbid(scoreQuery.getBjbh(), scoreQuery.getBdh(), scoreQuery.getZbid());
+        if (projectId == null || projectId <= 0) {
+            throw new RuntimeException("项目不存在");
+        }
+        EvaluationClause queryTkbm = new EvaluationClause();
+        queryTkbm.setClauseCode(scoreQuery.getTkbm());
+        EvaluationClause evaluationClause = evaluationClauseService.selectEvaluationClauseList(queryTkbm).stream().findFirst().orElse(null);
+        if (evaluationClause == null) {
+            // 条款不存在
+            throw new RuntimeException("条款不存在");
+        }
+        EvaluationResult params = new EvaluationResult();
+        params.setProjectId(projectId);
+        params.setClauseId(evaluationClause.getId());
+        EvaluationResult evaluationResult = evaluationResultService.selectEvaluationResultList(params).stream().findFirst().orElse(null);
+        if (evaluationResult != null) {
+            BidScoreVo bidScoreVo = new BidScoreVo();
+            bidScoreVo.setTkbm(evaluationClause.getClauseCode());
+//            bidScoreVo.setReviewFactorCode(evaluationResult.getOpinion());
+            bidScoreVo.setRuleRefinement(evaluationResult.getOpinion());
+            bidScoreVo.setAiScore(evaluationResult.getScore());
+//            bidScoreVo.setIsScore(evaluationResult.isScore());
+            bidScoreVo.setScoreBasis(evaluationResult.getBasis());
+            bidScoreVo.setTimestamp(DateUtils.getNowDate().getTime());
+            bidScoreVo.setSign(scoreQuery.getSign());
+            return bidScoreVo;
+        }
+        return null;
+    }
+
+    @Override
+    public BidScoreDetailVo query(BidScoreQuery scoreQuery) {
+        Long projectId = projectService.findProjectIdByBdhBjbhZbid(scoreQuery.getBjbh(), scoreQuery.getBdh(), scoreQuery.getZbid());
+        if (projectId == null || projectId <= 0) {
+            throw new RuntimeException("项目不存在");
+        }
+        List<ScoreDetail> scoreDetails = evaluationResultService.selectScoreDetailListByProjectId(projectId);
+        BidScoreDetailVo detailVo = new BidScoreDetailVo();
+        BeanUtils.copyProperties(scoreQuery, detailVo);
+        detailVo.setScoreDetails(scoreDetails);
+        detailVo.setTimestamp(DateUtils.getNowDate().getTime());
+        return detailVo;
+    }
